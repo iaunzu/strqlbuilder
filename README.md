f648086f28f344d0d13a1f53bb51cce6c5e2bfd1
Iñigo Alcalde Unzu <ialcalde@itracasa.es>
2017-10-06 11:45:57 +0200

    New release 1.1.1 with a bug fix.
    (*) Fix bug when using StrQLBuilder as parameter in conjunction with an
    IN clause.
    (+) Readme: first lines.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# strqlbuilder
+# StrQLBuilder
+SQL and PSQL builder focused on complex paginated queries
+
+## Description
+StrQLBuilder provides you a way to build native and PSQL queries using a fluent API and simplifying paging, COUNT queries and mapping to Data Transfer Object (DTO).
+
+Behind the scenes, StrQLBuilder uses [Hibernate](http://hibernate.org), so we can be sure a large number of databases are supported.
+
+## Usage
+Just include in your pom.xml file.
+```xml
+<dependency>
+    <groupId>com.github.iaunzu</groupId>
+    <artifactId>strqlbuilder</artifactId>
+    <version>1.1.1</version>
+</dependency>
+```
+
+### A brief example
+Let's start with a simple native query:
+```java
+@Autowired
+private EntityManager entityManager;
+
+public Long findIdById(Long id) {
+    TypedQuery<Long> query = StrQLBuilder.createNative()
+		.select("t.id")
+		.from("TestTable t")
+        .where("t.id = :param1", id)
+		.createQuery(entityManager, Long.class);
+	return query.getSingleResult();
+}
+```
+And that's it, now let's analyze it.
+First, you instantiate an StrQLBuilder and start concatenating SQL clauses.
+When you want to use a parameter, you simple declare it with a colon `:` prefix and add it to the
+Then, you call createQuery method and provide the EntityManager and the resulting DTO class (a primitive type in this case).
--- src/main/java/com/github/iaunzu/strqlbuilder/utils/ConditionValuesParser.java
+++ src/main/java/com/github/iaunzu/strqlbuilder/utils/ConditionValuesParser.java
@@ -7,115 +7,138 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class ConditionValuesParser {
+public class ConditionValuesParser
+{
 
-    ConditionValues conditionValues;
+	ConditionValues conditionValues;
 
-    private int IN_MAX_SIZE = 1000;
+	private int IN_MAX_SIZE = 1000;
 
-    public ConditionValuesParser(String condition, Object... values) {
-	this.conditionValues = new ConditionValues(condition, values);
-    }
+	public ConditionValuesParser(String condition, Object... values)
+	{
+		this.conditionValues = new ConditionValues(condition, values);
+	}
 
-    public ConditionValuesParser(ConditionValues conditionValues, int inMaxSize) {
-	this.conditionValues = conditionValues;
-	this.IN_MAX_SIZE = inMaxSize;
-    }
+	public ConditionValuesParser(ConditionValues conditionValues, int inMaxSize)
+	{
+		this.conditionValues = conditionValues;
+		this.IN_MAX_SIZE = inMaxSize;
+	}
 
-    public static ConditionValuesParser getInstance(String condition, Object... values) {
-	return new ConditionValuesParser(condition, values);
-    }
+	public static ConditionValuesParser getInstance(String condition, Object... values)
+	{
+		return new ConditionValuesParser(condition, values);
+	}
 
-    public ConditionValues parse() {
-	ConditionValues parsed = new ConditionValues();
-	String condition = clean(conditionValues.getCondition());
-	Matcher paramMatcher = Pattern.compile(":[^\\s\\)]+", Pattern.CASE_INSENSITIVE).matcher(condition);
+	public ConditionValues parse()
+	{
+		ConditionValues parsed = new ConditionValues();
+		String condition = clean(conditionValues.getCondition());
+		Matcher paramMatcher = Pattern.compile(":[^\\s\\)]+", Pattern.CASE_INSENSITIVE).matcher(condition);
 
-	int j = 0;
-	List<Object> list = new ArrayList<Object>(); // lista final values
-	Object[] values = conditionValues.getValues();
-	while (paramMatcher.find()) {
-	    String parameter = paramMatcher.group();
-	    Matcher subConditionMatcher = Pattern.compile("[^\\s\\(]+\\s+(?:not\\s+)?in\\s+" + Pattern.quote(parameter), Pattern.CASE_INSENSITIVE)
-		    .matcher(condition);
-	    Object subconditionValue = values[j]; // original value
-	    subconditionValue = convertToListIfArray(subconditionValue);
-	    boolean isInParameter = subConditionMatcher.find();
-	    if (isInParameter) {
-		String subcondition = subConditionMatcher.group();
-		if (!shouldSplit(subconditionValue)) {
-		    list.add(subconditionValue);
-		} else {
-		    // split
-		    List<?> listValue = (List<?>) subconditionValue;
-		    int chunks = splitValueIntoList(listValue, list);
+		int j = 0;
+		List<Object> list = new ArrayList<Object>(); // lista final values
+		Object[] values = conditionValues.getValues();
+		while (paramMatcher.find())
+		{
+			String parameter = paramMatcher.group();
+			Matcher subConditionMatcher = Pattern.compile("[^\\s\\(]+\\s+(?:not\\s+)?in\\s+" + Pattern.quote(parameter), Pattern.CASE_INSENSITIVE).matcher(condition);
+			Object subconditionValue = values[j]; // original value
+			subconditionValue = convertToListIfArray(subconditionValue);
+			boolean isInParameter = subConditionMatcher.find();
+			if (isInParameter)
+			{
+				String subcondition = subConditionMatcher.group();
+				if (!shouldSplit(subconditionValue))
+				{
+					list.add(subconditionValue);
+				}
+				else
+				{
+					// split
+					List<?> listValue = (List<?>)subconditionValue;
+					int chunks = splitValueIntoList(listValue, list);
 
-		    condition = StringUtils.replace(condition, subcondition, getReplacement(subcondition, chunks), 1);
+					condition = StringUtils.replace(condition, subcondition, getReplacement(subcondition, chunks), 1);
+				}
+			}
+			else
+			{
+				list.add(subconditionValue);
+			}
+			j++;
+
 		}
-	    } else {
-		list.add(subconditionValue);
-	    }
-	    j++;
+		parsed.setCondition(wrapInParametersWithParenthesis(condition));
+		parsed.setValues(list.toArray());
+		return parsed;
+	}
+
+	private String wrapInParametersWithParenthesis(String condition)
+	{
+		// This method prevents errors when list parameter is inside some parenthesis group in hibernate
+		return condition.replaceAll("(?i)(in)\\s+(:[^\\s\\)]+)", "$1 ($2)"); // replace "IN :list" --> "IN (:list)"
+	}
+
+	private Object convertToListIfArray(Object obj)
+	{
+		if (obj != null && obj.getClass().isArray())
+		{
+			return StrQLUtils.arrayToList(obj);
+		}
+		return obj;
 
 	}
-	parsed.setCondition(wrapInParametersWithParenthesis(condition));
-	parsed.setValues(list.toArray());
-	return parsed;
-    }
 
-    private String wrapInParametersWithParenthesis(String condition) {
-	// This method prevents errors when list parameter is inside some parenthesis group in hibernate
-	return condition.replaceAll("(?i)(in)\\s+(:[^\\s\\)]+)", "$1 ($2)"); // replace "IN :list" --> "IN (:list)"
-    }
+	private String getReplacement(String subcondition, int chunks)
+	{
+		boolean isNotIn = Pattern.compile("\\snot\\s", Pattern.CASE_INSENSITIVE).matcher(subcondition).find();
+		String logicalOperator = isNotIn ? " and " : " or ";
+		StringBuilder replacement = new StringBuilder("(").append(subcondition).append("__0");
+		for (int i = 1; i < chunks; i++)
+		{
+			replacement.append(logicalOperator).append(subcondition).append("__").append(i);
+		}
+		replacement.append(")");
+		return replacement.toString();
 
-    private Object convertToListIfArray(Object obj) {
-	if (obj != null && obj.getClass().isArray()) {
-	    return StrQLUtils.arrayToList(obj);
 	}
-	return obj;
 
-    }
-
-    private String getReplacement(String subcondition, int chunks) {
-	boolean isNotIn = Pattern.compile("\\snot\\s", Pattern.CASE_INSENSITIVE).matcher(subcondition).find();
-	String logicalOperator = isNotIn ? " and " : " or ";
-	StringBuilder replacement = new StringBuilder("(").append(subcondition).append("__0");
-	for (int i = 1; i < chunks; i++) {
-	    replacement.append(logicalOperator).append(subcondition).append("__").append(i);
+	private boolean shouldSplit(Object value)
+	{
+		if (!(value instanceof List))
+		{
+			return false;
+		}
+		List<?> listValue = (List<?>)value;
+		if (listValue.size() > IN_MAX_SIZE)
+		{
+			return true;
+		}
+		return false;
 	}
-	replacement.append(")");
-	return replacement.toString();
 
-    }
+	private int splitValueIntoList(List<?> value, List<Object> list)
+	{
+		int chunks = 0;
+		int sep = 0;
+		do
+		{
+			int fromIndex = sep;
+			int toIndex = Math.min(sep + IN_MAX_SIZE, value.size());
+			list.add(value.subList(fromIndex, toIndex));
+			sep += IN_MAX_SIZE;
+			chunks++;
+		}
+		while (sep < value.size());
 
-    private boolean shouldSplit(Object value) {
-	if (!(value instanceof List)) {
-	    return true;
+		return chunks;
 	}
-	List<?> listValue = (List<?>) value;
-	if (listValue.size() > IN_MAX_SIZE) {
-	    return true;
+
+	private String clean(String str)
+	{
+		str = str.replaceAll("[\"']", ""); // delete " and '
+		return str.replaceAll("\\(:(\\S+)\\)", ":$1"); // (:param) -> :param
 	}
-	return false;
-    }
-
-    private int splitValueIntoList(List<?> value, List<Object> list) {
-	int chunks = 0;
-	int sep = 0;
-	do {
-	    int fromIndex = sep;
-	    int toIndex = Math.min(sep + IN_MAX_SIZE, value.size());
-	    list.add(value.subList(fromIndex, toIndex));
-	    sep += IN_MAX_SIZE;
-	    chunks++;
-	} while (sep < value.size());
-
-	return chunks;
-    }
-
-    private String clean(String str) {
-	str = str.replaceAll("[\"']", ""); // delete " and '
-	return str.replaceAll("\\(:(\\S+)\\)", ":$1"); // (:param) -> :param
-    }
 
 }
